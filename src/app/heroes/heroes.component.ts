7d1932bdd73fbedd765e25a0fa0da7d46af0beac
dardoAleani <dlanda@aleni.com.ar>
2019-01-06 19:48:26 -0300

    Display list of heroes with hero style

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,6 +8,7 @@ import { RELAY_HEROES } from '../mock-heroes'
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
+
   /**
    * De esta manera, estamos declarando el nombre
    * de nuestro Héroe, pero solo lo estamos haciendo
@@ -20,25 +21,40 @@ export class HeroesComponent implements OnInit {
    * --> NOTAR: Estamos importando la clase RealHero,
    *            la cual tiene las propiedades del 
    *            objeto RealHero, que vamos a usar.
-  */
-
+   */
+  
   hero : RealHero = {
     id: 1,
     name: "Windstorm"
   }
-
+  
+  /**
+   * Creamos una variable de tipo: RealHero
+   * para poder tenerla en el "estado" de nuestro
+   * componente: heroes.component.ts
+   * De esta manera, nuestra variable: selectedHero,
+   * va a estar activa cada vez que se invoque desde 
+   * el evento -> click
+   */
+  selectedHero : RealHero
+  
   /**
    * De esta manaera, realacionamos la lista de 
-   * villanos con la variable: silverHeroes
+   * villanos con la variable: silverHeroes.
+   * Entonces tenemos un array en el cual podemos
+   * iterar en nuestro html --> heroes.component.html
    * 
    */
   silverHeroes = RELAY_HEROES
 
   constructor() { }
-
-
+  
+  
   ngOnInit() {
-    
+    console.log("testing element for console");
   }
-
+  
+  onSelect(silverHero: RealHero): void {
+    this.selectedHero = silverHero    
+  }
 }
