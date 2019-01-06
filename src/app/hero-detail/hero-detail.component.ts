956292bd97ae5ab3fdb0cc4b363fe7236705e835
dardoAleani <dlanda@aleni.com.ar>
2019-01-06 20:53:48 -0300

    Uncouple Details module - One way data binding

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core'
+import { RealHero } from '../realHero'
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.scss']
+})
+export class HeroDetailComponent implements OnInit {
+
+  @Input() genericHero: RealHero
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
